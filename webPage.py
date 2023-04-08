69f22f66e7ce322742e7e01a0a8fe6187e7c0404
TJA_07 <105525318+TJA07@users.noreply.github.com>
2023-04-08 11:46:02 -0700

    Add files via upload

---
+++ webPage.py
@@ -0,0 +1,82 @@
+import streamlit as st
+import numpy as np
+import plotly.figure_factory as ff
+
+
+def func(arg):
+    pass
+
+
+st.header("Map of deforestation")
+# Add histogram data
+x1 = np.random.randn(200) - 2
+x2 = np.random.randn(200)
+x3 = np.random.randn(200) + 2
+
+# Group data together
+hist_data = [x1, x2, x3]
+
+group_labels = ['Group 1', 'Group 2', 'Group 3']
+
+# Create distplot with custom bin_size
+fig = ff.create_distplot(
+        hist_data, group_labels, bin_size=[.1, .25, .5])
+
+# Plot!
+st.plotly_chart(fig, use_container_width=True)
+
+st.markdown(":blue[This graph depicts the loss of density in forests that has occured in the amazon.]")
+
+option = st.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone'))
+
+func(option)
+
+st.subheader("Progression of deforestation over time")
+x1 = np.random.randn(200) - 2
+x2 = np.random.randn(200)
+x3 = np.random.randn(200) + 2
+
+# Group data together
+hist_data = [x1, x2, x3]
+
+group_labels = ['Group 1', 'Group 2', 'Group 3']
+
+# Create distplot with custom bin_size
+fig = ff.create_distplot(
+        hist_data, group_labels, bin_size=[.1, .25, .5])
+
+# Plot!
+st.plotly_chart(fig, use_container_width=True)
+
+st.markdown(":blue[This graph depicts the loss of density in forests that has occured in the amazon.]")
+
+
+st.subheader("Forest fire map")
+
+x1 = np.random.randn(200) - 2
+x2 = np.random.randn(200)
+x3 = np.random.randn(200) + 2
+
+# Group data together
+hist_data = [x1, x2, x3]
+
+group_labels = ['Group 1', 'Group 2', 'Group 3']
+
+# Create distplot with custom bin_size
+fig = ff.create_distplot(
+        hist_data, group_labels, bin_size=[.1, .25, .5])
+
+# Plot!
+st.plotly_chart(fig, use_container_width=True)
+
+st.markdown(":blue[This graph depicts the loss of density in forests that has occured in the amazon.]")
+
+with st.expander("See explanation"):
+    st.write("""
+        The chart above shows some numbers I picked for you.
+        I rolled actual dice for these, so they're *guaranteed* to
+        be random.
+    """)
+    st.image("https://static.streamlit.io/examples/dice.jpg")
